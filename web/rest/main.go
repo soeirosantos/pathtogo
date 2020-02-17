936af3f3d800f2790eebc68946561d2c7959efda
Romulo Santos <soeirosantos@gmail.com>
2020-02-17 11:02:43 -0500

    api health check

--- web/rest/main.go
+++ web/rest/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+)
+
+type HealthCheck struct {
+	Status    string `json:"status"`
+	Timestamp int64  `json:"timestamp"`
+}
 
 func main() {
-	fmt.Println("foo")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/health", healthCheckHandler).Methods("GET")
+
+	log.Println("api started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(HealthCheck{Status: "OK",
+		Timestamp: time.Now().UnixNano() / 1000000})
 }
