a867d4d7629b6be29ae713acffc29f74b2f6db7a
Romulo Santos <soeirosantos@gmail.com>
2020-02-17 19:19:48 -0500

    partial articles api

--- web/rest/main.go
+++ web/rest/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -11,40 +13,41 @@ import (
 )
 
 func main() {
+
+	LoadMock()
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/health", HealthCheckHandler).Methods("GET")
 
-	router.HandleFunc("/articles", HealthCheckHandler).Methods("GET")
-	router.HandleFunc("/articles/{id}", HealthCheckHandler).Methods("GET")
-	router.HandleFunc("/articles", HealthCheckHandler).Methods("POST")
-	router.HandleFunc("/articles/{id}", HealthCheckHandler).Methods("PUT")
-	router.HandleFunc("/articles/{id}", HealthCheckHandler).Methods("DELETE")
-	router.HandleFunc("/articles/{id}/publish", HealthCheckHandler).
-		Methods("PATCH")
+	router.HandleFunc("/articles", ListArticleHandler).Methods("GET")
+	router.HandleFunc("/articles/{id}", GetArticleHandler).Methods("GET")
+	router.HandleFunc("/articles", AddArticleHandler).Methods("POST")
+	router.HandleFunc("/articles/{id}", UpdateArticleHandler).Methods("PUT")
+	router.HandleFunc("/articles/{id}", DeleteArticleHandler).Methods("DELETE")
+	router.HandleFunc("/articles/{id}", PublishArticleHandler).Methods("PATCH")
 
 	log.Println("api started on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 type Article struct {
-	ID            string    `json:"id"`
-	Title         string    `json:"title"`
-	Body          string    `json:"body"`
-	Published     bool      `json:"published"`
-	CreatedDate   time.Time `json:"created_date"`
-	UpdatedDate   time.Time `json:"updated_date"`
-	PublishedDate time.Time `json:"published_date"`
-	Authors       []string  `json:"authors`
+	ID            string     `json:"id"`
+	Title         string     `json:"title"`
+	Body          string     `json:"body"`
+	Published     bool       `json:"published"`
+	CreatedDate   time.Time  `json:"created_date"`
+	UpdatedDate   time.Time  `json:"updated_date"`
+	PublishedDate *time.Time `json:"published_date,omitempty"`
 }
 
 func newArticle(Title, Body string, Published bool) Article {
 	now := time.Now()
 	a := Article{ID: uuid.New().String(), Title: Title, Body: Body,
-		Published: Published, CreatedDate: now, PublishedDate: now}
+		Published: Published, CreatedDate: now, UpdatedDate: now}
 
 	if Published {
-		a.PublishedDate = now
+		a.PublishedDate = &now
 	}
 
 	return a
@@ -55,34 +58,202 @@ type HealthCheck struct {
 	Timestamp int64  `json:"timestamp"`
 }
 
+type ApiError struct {
+	ID        string `json:"id"`
+	Message   string `json:"message"`
+	Code      int    `json:"code"`
+	Timestamp int64  `json:"timestamp"`
+}
+
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	json.NewEncoder(w).Encode(HealthCheck{Status: "OK",
-		Timestamp: time.Now().UnixNano() / 1000000})
+		Timestamp: epochMilis()})
 }
 
 func ListArticleHandler(w http.ResponseWriter, r *http.Request) {
-
+	result, err := mockList()
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+	writeResponse(w, http.StatusOK, result)
 }
 
 func GetArticleHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 
+	exists, err := mockExists(id)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if !exists {
+		writeError(w, http.StatusNotFound, errors.New("not found"))
+		return
+	}
+
+	a, err := mockGet(id)
+
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	writeResponse(w, http.StatusOK, a)
 }
 
 func AddArticleHandler(w http.ResponseWriter, r *http.Request) {
+	var a Article
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&a); err != nil {
+		writeError(w, http.StatusBadRequest, errors.New("invalid request"))
+		return
+	}
 
+	if createdArticle, err := mockSave(a); err == nil {
+		writeResponse(w, http.StatusCreated, createdArticle)
+	} else {
+		writeError(w, http.StatusInternalServerError, err)
+	}
 }
 
 func UpdateArticleHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 
+	exists, err := mockExists(id)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if !exists {
+		writeError(w, http.StatusNotFound, errors.New("not found"))
+		return
+	}
+
+	var a Article
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&a); err != nil {
+		writeError(w, http.StatusBadRequest, errors.New("invalid request"))
+		return
+	}
+
+	if _, err := mockSave(a); err == nil {
+		writeResponse(w, http.StatusNoContent, nil)
+	} else {
+		writeError(w, http.StatusInternalServerError, err)
+	}
 }
 
 func PublishArticleHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 
+	exists, err := mockExists(id)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if !exists {
+		writeError(w, http.StatusNotFound, errors.New("not found"))
+		return
+	}
+
+	a, err := mockGet(id)
+
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	a.Published = true
+	now := time.Now()
+	a.PublishedDate = &now
+
+	if _, err := mockSave(*a); err == nil {
+		writeResponse(w, http.StatusNoContent, nil)
+	} else {
+		writeError(w, http.StatusInternalServerError, err)
+	}
 }
 
 func DeleteArticleHandler(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func writeResponse(w http.ResponseWriter, code int, payload interface{}) {
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(payload)
+	if err != nil {
+		log.Print(err.Error())
+	}
+}
+
+func writeError(w http.ResponseWriter, code int, err error) {
+	// if code is 5XX we shouldn't send details to the client in production
+	// skipping this check here
+	e := ApiError{ID: uuid.New().String(), Code: code, Message: err.Error(),
+		Timestamp: epochMilis()}
+	log.Printf("%s - %s", e.ID, err.Error())
+	writeResponse(w, code, e)
+}
+
+func epochMilis() int64 {
+	return time.Now().UnixNano() / 1000000
+}
+
+//////////
+// Mock //
+//////////
+
+var MockArticles map[string]Article
+
+func LoadMock() {
+	MockArticles = make(map[string]Article)
+	for i := 0; i < 10; i++ {
+		a :=
+			newArticle(fmt.Sprintf("Title %d", i), fmt.Sprintf("Body %d", i),
+				false)
+		MockArticles[a.ID] = a
+	}
+}
+
+func mockList() ([]Article, error) {
+	result := []Article{}
+	for _, v := range MockArticles {
+		result = append(result, v)
+	}
+	return result, nil
+}
+
+func mockExists(ID string) (bool, error) {
+	if _, ok := MockArticles[ID]; ok {
+		return true, nil
+	}
+	return false, nil
+}
+
+func mockGet(ID string) (*Article, error) {
+	if a, ok := MockArticles[ID]; ok {
+		return &a, nil
+	}
+	return nil, errors.New("no result error")
+}
+
+func mockSave(a Article) (*Article, error) {
+	MockArticles[a.ID] = a
+	return &a, nil
+}
+
+func mockDelete(ID string) error {
+	delete(MockArticles, ID)
+	return nil
 }
--- web/rest/main_test.go
+++ web/rest/main_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func Test_healthCheckHandler(t *testing.T) {
@@ -23,12 +27,190 @@ func Test_healthCheckHandler(t *testing.T) {
 	}
 
 	var hc HealthCheck
-	json.NewDecoder(rr.Body).Decode(&hc)
+	err = json.NewDecoder(rr.Body).Decode(&hc)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
 	expected := "OK"
 
 	if hc.Status != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			hc.Status, expected)
+	}
+}
+
+func TestListArticleHandler(t *testing.T) {
+	LoadMock()
+	req, _ := http.NewRequest("GET", "/articles", nil)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ListArticleHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	var articles []Article
+	err := json.NewDecoder(rr.Body).Decode(&articles)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if len(articles) == 0 {
+		t.Error("handler returned a wrong number of results body")
+	}
+}
+
+func TestGetArticleHandler(t *testing.T) {
+	LoadMock()
+
+	var id string
+	var expectedArticle Article
+	for k, a := range MockArticles {
+		id = k
+		expectedArticle = a
+		break
+	}
+
+	req, _ := http.NewRequest("GET", fmt.Sprintf("/articles/%s", id), nil)
+
+	rr := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/articles/{id}", GetArticleHandler)
+
+	router.ServeHTTP(rr, req)
+
+	var article Article
+	err := json.NewDecoder(rr.Body).Decode(&article)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if article.ID != expectedArticle.ID {
+		t.Errorf("expected id %s got %s", expectedArticle.ID, article.ID)
+	}
+}
+
+func TestAddArticleHandler(t *testing.T) {
+	LoadMock()
+
+	payload :=
+		[]byte(`{"title": "Test Article", "body": "Some body", "published": false}`)
+
+	req, _ := http.NewRequest("POST", "/articles", bytes.NewBuffer(payload))
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(AddArticleHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	var article Article
+	err := json.NewDecoder(rr.Body).Decode(&article)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if rr.Code != http.StatusCreated {
+		t.Errorf("Expected status code %d got %d", http.StatusCreated, rr.Code)
+		return
+	}
+
+	expected := "Test Article"
+
+	if article.Title != expected {
+		t.Errorf("Expected title %s got %s", expected, article.Title)
+	}
+}
+
+func TestUpdateArticleHandler(t *testing.T) {
+	LoadMock()
+
+	var id string
+	var originalArticle Article
+	for k, a := range MockArticles {
+		id = k
+		originalArticle = a
+		break
+	}
+
+	originalArticle.Title = "New Title"
+
+	payload, _ := json.Marshal(originalArticle)
+
+	req, _ := http.NewRequest("PUT", fmt.Sprintf("/articles/%s", id),
+		bytes.NewBuffer(payload))
+
+	rr := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/articles/{id}", UpdateArticleHandler)
+
+	router.ServeHTTP(rr, req)
+
+	changedArticle := MockArticles[id]
+
+	if rr.Code != http.StatusNoContent {
+		t.Errorf("Expected status code %d got %d", http.StatusNoContent, rr.Code)
+		return
+	}
+
+	if changedArticle.ID != originalArticle.ID {
+		t.Errorf("expected id %s got %s", originalArticle.ID, changedArticle.ID)
+		return
+	}
+
+	if changedArticle.Title != originalArticle.Title {
+		t.Errorf("expected title %s got %s", originalArticle.ID, changedArticle.ID)
+		return
+	}
+
+	if changedArticle.Body != originalArticle.Body {
+		t.Errorf("expected body %s got %s", originalArticle.Body, changedArticle.Body)
+	}
+}
+
+func TestPublishArticleHandler(t *testing.T) {
+	LoadMock()
+
+	var id string
+	var originalArticle Article
+	for k, a := range MockArticles {
+		id = k
+		originalArticle = a
+		break
+	}
+
+	req, _ := http.NewRequest("PATCH", fmt.Sprintf("/articles/%s", id), nil)
+
+	rr := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/articles/{id}", PublishArticleHandler)
+
+	router.ServeHTTP(rr, req)
+
+	publishedArticle := MockArticles[id]
+
+	if rr.Code != http.StatusNoContent {
+		t.Errorf("Expected status code %d got %d", http.StatusNoContent, rr.Code)
+		return
+	}
+
+	if publishedArticle.ID != originalArticle.ID {
+		t.Errorf("expected id %s got %s", originalArticle.ID, publishedArticle.ID)
+		return
+	}
+
+	if publishedArticle.Title != originalArticle.Title {
+		t.Errorf("expected title %s got %s", originalArticle.ID, publishedArticle.ID)
+		return
+	}
+
+	if !publishedArticle.Published {
+		t.Errorf("expected published %v got %v", true, publishedArticle.Published)
 	}
 }
